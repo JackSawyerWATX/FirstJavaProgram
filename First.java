c69ca4626974655ebe3a942fc28d9ae1aee6957b
Jack Sawyer <jack.h.sawyer@pm.me>
2022-01-06 18:07:57 -0600

    first commit

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is Jonathan.");
+        System.out.println("I am 45 years old.");
+        System.out.println("I am from Amarillo, Texas.");
+    }
+}
